5a409284d4de9ec680a586ccb3891366e7316465
Yernur Abralin <y_abralin@kbtu.kz>
2024-02-06 13:45:05 +0600

    2nd lab

---
+++ lab2/sets.py
@@ -0,0 +1,13 @@
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
